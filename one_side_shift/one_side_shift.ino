1873300864a99da4a2265ae982524bbaaf6438bc
Achintya Saxena <81406743+Dadaji18@users.noreply.github.com>
2021-07-22 09:19:33 +0530

    Add files via upload

--- one_side_shift/one_side_shift.ino
+++ one_side_shift/one_side_shift.ino
@@ -4,7 +4,9 @@
 
 void store();
 int pic[] = {0,36,36,36,0,66,60,0};
-int A[] = {24,60,102,102,126,126,102,102};
+int A[] = {24,60,102,102,126,102,102,102};
+int B[] = {124,102,102,124,126,102,102,124};
+int test[] = {2,2,2,2,2,2,2,2};
 
 void setup() {
   pinMode(DATA, OUTPUT);
@@ -13,15 +15,15 @@ void setup() {
 }
 
 void loop() {
-  for (int j=0; j<8; j++) {
+//  for (int j=0; j<8; j++) {
     for (int i=0; i<8; i++) {
-      shiftOut(DATA, SHIFT, LSBFIRST, ~pic[i] >> j);
+      shiftOut(DATA, SHIFT, LSBFIRST, ~B[i]);
       shiftOut(DATA, SHIFT, LSBFIRST, 128 >> i);
       store();
       delay(6);
     }
-    delay(12);
-  }
+//    delay(12);
+//  }
 }
 
 void store() {
